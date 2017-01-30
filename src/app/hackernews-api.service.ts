0daaff8962504f3a5c3344cd1b8103b3a908993d
cajoue <cajoue@users.noreply.github.com>
2017-01-30 16:07:17 +0100

    refactor: Switch API for faster load time
    The separate calls for each item in the parent list made for slow
    load times. A different API is used that brings in the child data at
    the same time as the parent list. Load time is improved.

--- src/app/hackernews-api.service.ts
+++ src/app/hackernews-api.service.ts
@@ -8,16 +8,11 @@ export class HackernewsApiService {
   baseUrl: string;
 
   constructor(private http: Http) {
-    this.baseUrl = 'https://hacker-news.firebaseio.com/v0';
+    this.baseUrl = 'https://node-hnapi.herokuapp.com';
   }
 
-  fetchStories(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/topstories.json`)
-                    .map(response => response.json());
-  }
-
-  fetchItem(id: number): Observable<any> {
-    return this.http.get(`${this.baseUrl}/item/${id}.json`)
+  fetchStories(storyType: string, page: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${storyType}?page=${page}`)
                     .map(response => response.json());
   }
 }
